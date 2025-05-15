289406214649c2162e39801c2c3d0b6ebef0725a
Murrrphy <444575716@qq.com>
2025-05-15 20:05:55 +0800

    完成alt_alloc

--- arceos/modules/bump_allocator/src/lib.rs
+++ arceos/modules/bump_allocator/src/lib.rs
@@ -1,8 +1,7 @@
 #![no_std]
 
-use core::ptr::NonNull;
-
 use allocator::{AllocError, BaseAllocator, ByteAllocator, PageAllocator};
+use core::ptr::NonNull;
 
 /// Early memory allocator
 /// Use it before formal bytes-allocator and pages-allocator can work!
@@ -18,57 +17,58 @@ use allocator::{AllocError, BaseAllocator, ByteAllocator, PageAllocator};
 /// When it goes down to ZERO, free bytes-used area.
 /// For pages area, it will never be freed!
 ///
-pub struct EarlyAllocator {
-    start: usize,
-    size: usize,
+pub struct EarlyAllocator<const PAGE_SIZE: usize> {
     byte_end: usize,
     page_end: usize,
+    count: usize,
+    start: usize,
+    size: usize,
 }
 
-impl EarlyAllocator {
+impl<const PAGE_SIZE: usize> EarlyAllocator<PAGE_SIZE> {
     pub const fn new() -> Self {
         Self {
-            start: 0,
-            size: 0,
             byte_end: 0,
             page_end: 0,
+            count: 0,
+            start: 0,
+            size: 0,
         }
     }
 }
 
-impl BaseAllocator for EarlyAllocator {
+impl<const PAGE_SIZE: usize> BaseAllocator for EarlyAllocator<PAGE_SIZE> {
     fn init(&mut self, start: usize, size: usize) {
-        let mut allocator = Self::new();
-        allocator.start = start;
-        allocator.size = size;
-        allocator.byte_end = 0;
-        allocator.page_end = start + size;
+        self.byte_end = start;
+        self.page_end = start + size;
+        self.count = 0;
+        self.start = start;
+        self.size = size;
     }
-    fn add_memory(&mut self, start: usize, size: usize) -> allocator::AllocResult {
-        // if self.start + self.size != start {
-        //     return Err(AllocError::InvalidParam);
-        // }
-        // self.size += size;
-        // self.page_end += size;
 
-        // Ok(())
-        todo!()
+    fn add_memory(&mut self, start: usize, size: usize) -> allocator::AllocResult {
+        unimplemented!()
     }
 }
 
-impl ByteAllocator for EarlyAllocator {
-    fn alloc(&mut self, layout: core::alloc::Layout) -> allocator::AllocResult<core::ptr::NonNull<u8>> {
+impl<const PAGE_SIZE: usize> ByteAllocator for EarlyAllocator<PAGE_SIZE> {
+    fn alloc(
+        &mut self,
+        layout: core::alloc::Layout,
+    ) -> allocator::AllocResult<core::ptr::NonNull<u8>> {
         let align = layout.align();
         let start = self.byte_end.next_multiple_of(align);
         self.byte_end = start + layout.size();
         if self.byte_end > self.page_end {
             return Err(AllocError::NoMemory);
         }
-
         unsafe { Ok(NonNull::new_unchecked(start as *mut u8)) }
     }
     fn dealloc(&mut self, pos: core::ptr::NonNull<u8>, layout: core::alloc::Layout) {
-        todo!();
+        self.count = self.count - 1;
+        if self.count == 0 {
+            self.byte_end = self.start;
+        }
     }
     fn total_bytes(&self) -> usize {
         self.size
@@ -81,25 +81,37 @@ impl ByteAllocator for EarlyAllocator {
     }
 }
 
-impl<const PAGE_SIZE: usize> PageAllocator for EarlyAllocator {
+impl<const PAGE_SIZE: usize> PageAllocator for EarlyAllocator<PAGE_SIZE> {
     const PAGE_SIZE: usize = PAGE_SIZE;
 
-    fn alloc_pages(&mut self, num_pages: usize, align_pow2: usize) -> allocator::AllocResult<usize> {
-        if num_pages > self.available_bytes() {
+    fn alloc_pages(
+        &mut self,
+        num_pages: usize,
+        align_pow2: usize,
+    ) -> allocator::AllocResult<usize> {
+        if self.count == 0 {
+            self.page_end = self.page_end - num_pages * PAGE_SIZE;
+            self.count = num_pages;
+        }
+        if self.count == 0 {
             return Err(AllocError::NoMemory);
         }
-        
+        self.count = self.count - 1;
+        Ok(self.page_end)
     }
     fn dealloc_pages(&mut self, pos: usize, num_pages: usize) {
-        todo!();
+        self.count = self.count + 1;
+        if self.count == 0 {
+            self.page_end = self.page_end + num_pages * PAGE_SIZE;
+        }
     }
     fn total_pages(&self) -> usize {
-        self.size / PAGE_SIZE;
+        self.size / PAGE_SIZE
     }
     fn used_pages(&self) -> usize {
-        self.total_pages() - self.available_pages();
+        self.total_pages() - self.available_pages()
     }
     fn available_pages(&self) -> usize {
-        (self.page_end - self.byte_end) / PAGE_SIZE;
+        (self.page_end - self.byte_end) / PAGE_SIZE
     }
 }
